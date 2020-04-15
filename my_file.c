bc74484d0a1e6a321155ca9b083f4202fbeef021
Sh995-web <shreyes3112@gmail.com>
2020-04-15 06:12:06 +0530

    file open

--- my_file.c
+++ my_file.c
@@ -3,18 +3,22 @@ int main()
 {
 int i,n=2;
 char str[50];
-FILE *fptr=fopen("sample.txt","w+");
-if(fptr==NULL);
+FILE *fptr=NULL;
+fptr=fopen("sample.txt","w+");
+
+if(fptr==NULL)
 {
 printf("could not openfile");
 return 0;
 }
+
 for(i=0;i<n;i++)
 {
 puts("enter a name");
 scanf("%s",str);
 fprintf(fptr,"%d,%s\n",i,str);
 }
+
 fclose(fptr);
 return 0;
 }
